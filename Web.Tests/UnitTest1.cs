23022a7d218f823f6516aed19f56a65aaab75254
Pencho Ilchev <larvalis@gmail.com>
2012-11-19 23:28:02 +0000

    adding web.tests project

---
+++ Web.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Web.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Web.Controllers;
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();  
+        }
+    }
+}
